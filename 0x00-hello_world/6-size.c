64f1ac2227ccfc97485db8801c6820b04cd9c40a
abdiwoli <abdiwolih@gmail.com>
2023-06-15 13:30:12 -0700

    test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,6 @@
 int main(void)
 {
 printf("Size of char: %zu byte(s)\n", sizeof(char));
-printf("Size of short: %zu byte(s)\n", sizeof(short));
 printf("Size of int: %zu byte(s)\n", sizeof(int));
 printf("Size of long: %zu byte(s)\n", sizeof(long));
 printf("Size of long long: %zu byte(s)\n", sizeof(long long));
