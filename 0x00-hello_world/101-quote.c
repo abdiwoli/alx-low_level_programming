07449c09804e298614e6e0416b8e229ac2a5a308
abdiwoli <abdiwolih@gmail.com>
2023-06-15 15:48:12 -0700

    finish

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdion.h>
+/**
+* main - function is for learnung purpose
+* Return: returns 0
+*/
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+return (1);
+}
