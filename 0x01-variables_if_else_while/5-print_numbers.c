9aad5c00cb0a2b892bac73a6f0792e987a6d708d
abdiwoli <abdiwolih@gmail.com>
2023-06-16 15:53:01 -0700

    five

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - is the only function
+ * Return: always 0
+ * author - by abdiwoli hassen
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 10; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
