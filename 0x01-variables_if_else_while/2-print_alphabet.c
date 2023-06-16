001b5150184dcfbc9526214a0c485befcfab9f82
abdiwoli <abdiwolih@gmail.com>
2023-06-16 14:06:05 -0700

    show

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+* main - function prints small alphabets
+* return:  return 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; a <= 'z'; c++;
+putchar(c);
+putchar('\n');
+return (0);
+}
