0aa955a2ab7910547fdc26f42ebe784ca6734fda
abdiwoli <abdiwolih@gmail.com>
2023-06-16 17:52:02 -0700

    tired

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,19 +5,15 @@
 */
 int main(void)
 {
-	int c, i;
+	int c;
 
-	char *arr[2];
-
-	arr[0] = ",";
-	arr[1] = " ";
 	for (c = 0; c < 10; c++)
 	{
 		putchar(c + '0');
 		if (c != 9)
 		{
-			for (i = 0; i < 2; i++)
-				putchar(*arr[i]);
+			putchar(',');
+			putchar(' ');
 		}
 
 	}
