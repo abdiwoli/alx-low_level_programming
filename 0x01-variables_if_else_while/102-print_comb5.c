2b25ca621bd4f7f07c0d659be8ac35157ee489ee
abdiwoli <abdiwolih@gmail.com>
2023-06-17 10:15:46 -0700

    finish

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,44 +6,25 @@
  */
 int main(void)
 {
-	int one, two, f, d1, d2;
+	int one, two;
 
-	f = 0;
 	for (one = 0; one < 99; one++)
 	{
 		for (two = one + 1; two <= 99; two++)
 		{
-			if (one < 10)
-			{
-				putchar(f + '0');
-				putchar(one + '0');
-			}
-			else
-			{
-				d1 = one / 10;
-				d2 = one % 10;
-				putchar(d1 + '0');
-				putchar(d2 + '0');
-			}
+			putchar((one / 10) + '0');
+			putchar((one % 10) + '0');
 			putchar(',');
 			putchar(' ');
-			if (two < 10)
-			{
-				putchar(one + '0');
-				putchar(two + '0');
-			}
-			else
-			{
-				d1 = two / 10;
-				d2 = two % 10;
-				putchar(d1 + '0');
-				putchar(d2 + '0');
-			}
+			putchar((two / 10) + '0');
+			putchar((two % 10) + '0');
 
-			putchar(',');
-			putchar(' ');
+			if (!(one == 98 && two == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-
 	}
 
 	return (0);
