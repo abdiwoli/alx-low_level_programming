c5ab9c74b58731632b799fbc4ec135290d117e6a
abdiwoli <abdiwolih@gmail.com>
2023-06-20 09:00:20 -0700

    done first one"

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,25 @@
-#include<stdion.h>
+#include <unistd.h>
 /**
  * putcharprinter - is a function that prints _putchar
  * Return: retruns 0
- */
-#include <unistd.h>
+*/
 
-void _putchar(char c) {
+void _putchar(char c)
+{
 	write(1, &c, 1);
+}
+
+/**
+ * main - main function to cal other functions
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char *text = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(text[i]);
+	return (0);
 }
