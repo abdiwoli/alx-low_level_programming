4b79bad9a36d6ed9fa0964f8808ece2b188fe17d
abdiwoli <abdiwolih@gmail.com>
2023-06-21 16:58:10 -0700

    doneadvnce1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,7 +17,11 @@ void print_times_table(int num)
         n2 = 0;
         for (i = 0; i <= num; i++)
         {
+		if (num < 0 || num > 15)
+			break;
                 putchar(0 + '0');
+		if (num > 0)
+			break;
                 spaceComa();
                 for (n = 1; n <= num; n++)
                 {
