2cce934b076a6bb457aa8a52fbe676704c34b8d0
abdiwoli <abdiwolih@gmail.com>
2023-06-21 06:10:11 -0700

    done7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
-/**                                                                                                                          
- * _abs - gets the absolute number                                                                                           
- * Return: absolute int                                                                                                      
- * @n: takes an int number                                                                                                   
- * author abdiwoli                                                                                                           
+/**
+ * _abss - gets the absolute number
+ * Return: absolute int
+ * @n: takes an int number
+ * author abdiwoli
 */
 
 int _abss(int n)
 {
-        return (n < 0 ? n * -1 : n > 0 ? n : 0);
+	return (n < 0 ? n * -1 : n > 0 ? n : 0);
 }
 
 /**
@@ -21,6 +21,7 @@ int _abss(int n)
 int print_last_digit(int n)
 {
 	int lastD;
+
 	lastD = _abss(n) % 10;
 	_putchar(lastD + '0');
 	return (lastD);
