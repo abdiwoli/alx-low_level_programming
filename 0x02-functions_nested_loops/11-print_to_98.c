bbf6340b237cb1b881dd0e52cb440e53217a2ebc
abdiwoli <abdiwolih@gmail.com>
2023-06-20 18:20:52 -0700

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,10 +8,10 @@ void print_to_98(int n)
 {
 	int one, two;
 
-	for (; n <= 98? n <= 98: n > 98;  n > 98 ? n--: n++)
+	for (; n <=98;  n > 98 ? n--: n++)
 	{
-	        two = n % 10;
-		one = n / 10;
+	        two = _abs( n) % 10;
+		one = _abs(n) / 10;
 		if (_abs(n) > 9)
 		      _putchar(one + '0');
 		_putchar(two + '0');
