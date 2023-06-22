4bf07e27c1c015579e931e74cec5092f754bbf22
abdiwoli <abdiwolih@gmail.com>
2023-06-21 17:23:06 -0700

    doneadvnce1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,44 +19,45 @@ void print_times_table(int num)
 
 	n2 = 0;
 	for (i = 0; i <= num; i++)
-        {
+	{
 		if (num < 0 || num > 15)
 			break;
-                putchar(0 + '0');
+		putchar(0 + '0');
 		if (num == 0)
+		{
+			putchar(' ');
 			break;
-                spaceComa();
-                for (n = 1; n <= num; n++)
-                {
-                        n2 = (n * i);
-			 if (n2 > 99)
-                        {
-                                n4 = (n2 / 10) % 10;
-                                n3 = n2 % 10;
-                                n2 = 1;
-                                putchar(n2 + '0');
-                                putchar(n4 + '0');
-                                putchar(n3 + '0');
-                        }
-			 else if (n2 > 9)
-                        {
-				
+		}
+		spaceComa();
+		for (n = 1; n <= num; n++)
+		{
+			n2 = (n * i);
+			if (n2 > 99)
+			{
+				n4 = (n2 / 10) % 10;
+				n3 = n2 % 10;
+				n2 = 1;
+				putchar(n2 + '0');
+				putchar(n4 + '0');
+				putchar(n3 + '0');
+			}
+			else if (n2 > 9)
+			{
 				putchar(' ');
-                                n3 = n2 / 10;
-                                n2 = n2 % 10;
-                                putchar(n3 + '0');
-                                putchar(n2 + '0');
-                        }
-                        else
-                        {
+				n3 = n2 / 10;
+				n2 = n2 % 10;
+				putchar(n3 + '0');
+				putchar(n2 + '0');
+			}
+			else
+			{
 				putchar(' ');
 				putchar(' ');
-                                putchar(n2 + '0');
-                        }
-                        if (n != num)
-                                spaceComa();
-                }
-                putchar('\n');
-        }
-	
+				putchar(n2 + '0');
+			}
+			if (n != num)
+				spaceComa();
+		}
+		putchar('\n');
+	}
 }
