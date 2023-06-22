88e6710efb0b4fd3d712404d1b6b73f906eb6639
abdiwoli <abdiwolih@gmail.com>
2023-06-22 13:39:08 -0700

    01

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * _isupper - function uppercase
- * Return : 1 or 0
+ * Return: 1 or 0
  * @c: int param
 */
 
 int _isupper(int c)
 {
-	return (c<= 97 ? 1 : 0);
+	return (c <= 97 ? 1 : 0);
 }
