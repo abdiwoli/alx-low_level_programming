1ca7161521ef1577ded5cf74412afa600dc4421a
abdiwoli <abdiwolih@gmail.com>
2023-06-26 22:35:38 +0300

    done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,13 +1,28 @@
 #include "main.h"
+#include <stdio.h>
+int abdi(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
 /**
- * rev_string -revers string
- *@s: string param
+ * rev_string - reverses the string string param
  */
 void rev_string(char *s)
 {
 	int i;
 
-	i = 0;
-	while (i < _strlen(s))
-	      _putchar(s[i++]);
+	i = abdi(s);
+	while (i >= 0)
+		putchar(s[--i]);
+	putchar('\n');
+	
+	
 }
