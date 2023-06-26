3ca6d84515c5a2e4c0ab97044739e2b2377ebcef
abdiwoli <abdiwolih@gmail.com>
2023-06-26 23:44:07 +0300

    done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - prints even chars
+ * @str: string param
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
