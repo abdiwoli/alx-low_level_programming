783275ac86cd5d84718b2b058231d871b64ee6a2
abdiwoli <abdiwolih@gmail.com>
2023-06-26 15:54:57 +0300

    done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+# include "main.h"
+/**
+ * _strlen - returns length of param
+ * @s: string pram
+ * Return: int type
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while (str[i] != '\0')
+		i++;
+	return i;
+}
