b36e68ae6fbc9eb3314baef06a0feb52cba752a2
abdiwoli <abdiwolih@gmail.com>
2023-06-27 05:31:47 +0300

    done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -9,22 +10,17 @@ int _atoi(char *s)
 {
 	int result = 0;
 	int sign = 1;
-
-	if (*s == '-')
+	while(*s != '\0')
 	{
-		sign = -1;
-		s++;
-	}
-
-	while (*s != '\0')
-	{
-		if (*s >= '0' && *s <= '9')
+		if(*s == '-')
+			sign = -1;
+		if(*s >= '0' && *s <= '9')
 		{
 			result = result * 10 + (*s - '0');
 		}
+		else if(result > 0)
+			break;
 		s++;
 	}
-
 	return (result * sign);
 }
-
