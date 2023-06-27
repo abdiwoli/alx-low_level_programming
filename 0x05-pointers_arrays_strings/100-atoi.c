294d8eedfee82a1fa7bbd0e9814b05804d88a596
abdiwoli <abdiwolih@gmail.com>
2023-06-27 04:19:38 +0300

    done

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _atoi - function
+ * @s: sting param
+ * Return: retrun resuls int
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+
+	if (*s == '-')
+	{
+		sign = -1;
+		s++;
+	}
+
+	while (*s != '\0')
+	{
+		if (*s >= '0' && *s <= '9')
+		{
+			result = result * 10 + (*s - '0');
+		}
+		s++;
+	}
+
+	return (result * sign);
+}
+
