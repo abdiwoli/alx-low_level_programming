16f3dd85badb3ef983adcd38c581d9bda86e886b
abdiwoli <abdiwolih@gmail.com>
2023-06-29 21:26:18 +0300

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,11 @@ char *_strcat(char *dest, char *src)
 {
 	while (*dest != '\0')
 		dest++;
-	*dest = ' ';
 	while (*src != '\0')
 	{
 		*dest = *src;
 		src++;
+		dest++;
 	}
 	*dest = '\0';
 	return (dest);
