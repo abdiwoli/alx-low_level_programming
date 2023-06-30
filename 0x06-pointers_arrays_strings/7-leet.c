b279ff3fc9b9db38dbce8cffc826746419db4fbd
abduwoli <abdiwolih@gmail.com>
2023-07-01 01:51:30 +0300

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,39 +1,27 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * leet - function that encodes a string into 1337.
- * @s: string
- * Return: char
+ * leet - change vowels to numbers.
+ * @s: analized string.
+ *
+ * Return: String with all vowels changed.
  */
 char *leet(char *s)
 {
-	int i;
+char a[] = "aeotlAEOTL";
+char n[] = "4307143071";
+int i = 0;
+int j;
 
-	DictionaryEntry dictionary[] = {
-		{'a', '4'},
-		{'A', '4'},
-		{'e', '3'},
-		{'E', '3'},
-		{'o', '0'},
-		{'O', '0'},
-		{'t', '7'},
-		{'T', '7'},
-		{'l', '1'}
-	};
-
-	while (*s != '\0')
-	{
-		for (i = 0; i < 9; i++)
-		{
-			if (dictionary[i].letter == *s)
-			{
-				*s = dictionary[i].value;
-				break;
-			}
-		}
-		s++;
-	}
-
-	return (s);
+while (*(s + i) != '\0')
+{
+for (j = 0; j <= 9; j++)
+{
+if (*(s + i) == a[j])
+{
+*(s + i) = n[j];
+}
+}
+i++;
+}
+return (s);
 }
