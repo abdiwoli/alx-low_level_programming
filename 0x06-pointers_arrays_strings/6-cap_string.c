fbc253086dc666112fbb43e8fff840c8b6a06a1f
abdiwoli <abdiwolih@gmail.com>
2023-06-30 05:10:31 +0300

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,38 @@
 #include "main.h"
 /**
- * string_toupper - a function that changes all lowercase
+ * cap_string - a function that changes all lowercase
  * @s: sting
  * Return: retuns char string
  */
 char *cap_string(char *s)
 {
 	char *tmp = s;
+	int capitalize_next = 1;
 
 	while (*tmp != '\0')
 	{
+		if (capitalize_next && (*tmp >= 'a' && *tmp <= 'z'))
+		{
+			*tmp = *tmp - 32;
+		}
+
+		capitalize_next = 0;
+
 		if (*tmp == ' ' || *tmp == '\t' || *tmp == '\n')
 		{
-			tmp++;
-			if (*tmp >= 'a' && *tmp <= 'z')
-				*tmp = *tmp - 32;
+			capitalize_next = 1;
 		}
-		else
-			tmp++;
-	}
-	if (*s >= 'a' && *s <= 'z')
-		*s = *s + 32;
-	return (s);
 
+		if (*tmp == '\t')
+		{
+			*tmp = ' ';
+			capitalize_next = 1;
+		}
+
+		tmp++;
+	}
+
+	return (s);
 }
+
+
