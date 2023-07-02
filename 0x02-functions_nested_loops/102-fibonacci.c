6c9c1950897938fd0cd1cbd03ef427cda3edb4bc
abdiwoli <abdiwolih@gmail.com>
2023-07-03 00:04:19 +0300

    done2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,13 +8,15 @@ int main(void)
 {
 	int next = 1;
 	int prev = 0;
+	int temp;
+
 	char *c = ", ";
 
 	for (int i = 1; i < 50; i++)
 	{
 		if (i == 49)
 			c = "\n";
-		int temp = next;
+		temp = next;
 		next = next + prev;
 		printf("%d%s", next, c);
 		prev = temp;
