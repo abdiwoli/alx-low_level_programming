fa40075c8e8fcea83d7fdb1a17997310c787cdb7
abdiwoli <abdiwolih@gmail.com>
2023-07-03 01:51:26 +0300

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
-#include <gmp.h>
+/**
+ * main - main fun
+ * Return: 0
+ */
 
 int main(void)
 {
-	mpz_t next, prev, temp;
+	unsigned long next = 1;
+	unsigned long prev = 0;
+	unsigned long temp;
 	int i;
 
 	char *c = ", ";
-
-	mpz_set_ui(next, 1);
-	mpz_set_ui(prev, 0);
-	mpz_init(temp);
 
 	for (i = 1; i <= 98; i++)
 	{
 		if (i == 98)
 			c = "\n";
-
-		mpz_add(temp, next, prev);
-		gmp_printf("%Zd%s", temp, c);
-
-		mpz_set(prev, next);
-		mpz_set(next, temp);
+		temp = next;
+		next = next + prev;
+		printf("%lu%s", next, c);
+		prev = temp;
 	}
 
-	mpz_clear(next);
-	mpz_clear(prev);
-	mpz_clear(temp);
-
-	return 0;
+	return (0);
 }
