c9aaf67bbffce43528d12821bdf3f912614c1ecd
abdiwoli <abdiwolih@gmail.com>
2023-07-04 23:53:00 +0300

    done

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,34 +7,25 @@
  * @needle: the string to search
  * Return: returns pointer
  */
-char *_strstr(char *haystack, char *needle)
-{
+char *_strstr(char *haystack, char *needle) {
 	int i, j, len;
 
 	len = strlen(needle);
 	if (len == 0)
-		return (needle);
-	i = 0;
-	while (*(needle + i) != '\0')
-	{
-		j = 0;
-		while (*(haystack + j) != '\0')
-		{
-			int k;
+		return haystack;
 
-			if (*(needle + i) == *(haystack + j))
-			{
-				for (k = 0; k < len; k++)
-				{
-					if (*(needle + i + k) != *(haystack + j + k))
-						break;
-				}
-				if (k == len)
-					return (haystack + j);
-			}
+	i = 0;
+	while (haystack[i] != '\0') {
+		j = 0;
+		while (needle[j] != '\0') {
+			if (haystack[i + j] != needle[j])
+				break;
 			j++;
 		}
+		if (needle[j] == '\0')
+			return (haystack + i);
 		i++;
 	}
-	return (0);
+
+	return (NULL);
 }
