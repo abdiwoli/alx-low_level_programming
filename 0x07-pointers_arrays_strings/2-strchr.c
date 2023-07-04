734dfe36f21d490378e1f248176f002d19d69062
abdiwoli <abdiwolih@gmail.com>
2023-07-04 05:03:42 +0300

    doneQ2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,11 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	i = 0;
-	while (*(s + i))
+	while (*s != '\0')
 	{
-		if (*(s + i) == c)
-			return ((s + i));
-		i++;
+		if (*s == c)
+			return (s);
+		s++;
 	}
 	if (*s == c)
 		return (s);
