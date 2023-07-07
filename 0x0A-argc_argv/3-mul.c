d24731e1511ceee6e1b0435880f34dde6593ac13
abdiwoli <abdiwolih@gmail.com>
2023-07-07 21:48:27 +0300

    done3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function prints all arguments
+ * @argc: int the size of the array
+ * @argv: the array
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	char *endptr, *endptr1;
+
+	if (argc >= 3)
+	{
+		int num = strtol(argv[1], &endptr, 10);
+		int num1 = strtol(argv[2], &endptr1, 10);
+
+		if (num)
+			printf("%d\n", num + num1);
+		else
+			printf("%d\n", num1);
+	}
+	else
+		printf("Error\n");
+	return (argc + 1);
+}
