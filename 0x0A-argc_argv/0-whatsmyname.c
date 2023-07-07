7115ec771ab2aa9a631f8d6761c19ca628c4f565
abdiwoli <abdiwolih@gmail.com>
2023-07-07 18:26:06 +0300

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function that prints program name
+ * @argc: int param the size of array
+ * @argv: array of arguments
+ * Return: the size
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (argc);
+}
