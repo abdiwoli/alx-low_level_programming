4d3c97828b3b19e54fbd632bca93de683e2efad0
abdiwoli <abdiwolih@gmail.com>
2023-07-08 02:42:29 +0300

    done

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strcat - a function that concatenates two strings
+ *@dest: appens other srring
+ *@src: another string
+ * Return: pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *tmp = dest;
+
+	while (*tmp != '\0')
+		tmp++;
+	while (*src != '\0')
+	{
+		*tmp = *src;
+		src++;
+		tmp++;
+	}
+	*tmp = '\0';
+	return (dest);
+}
