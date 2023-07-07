1d65aca9bca8cbbaa1eded25f5e231402da4a2e6
abdiwoli <abdiwolih@gmail.com>
2023-07-07 21:13:49 +0300

    done0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,25 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _puts_recursion - prints string
+ * @s: string paramater
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		write(1, &(*s), 1);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+	{
+		char c = '\n';
+
+		write(1, &c, 1);
+	}
+}
 /**
  * main - main function that prints program name
  * @argc: int param the size of array
@@ -8,6 +28,6 @@
  */
 int main(int argc, char **argv)
 {
-	printf("%s\n", *argv);
+	_puts_recursion(*argv);
 	return (argc);
 }
