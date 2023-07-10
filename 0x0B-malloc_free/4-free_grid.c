f5e99be453914a471accb320e069bd24cf9ee5c1
abdiwoli <abdiwolih@gmail.com>
2023-07-10 23:01:12 +0300

    done

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
+
+int get_width(int** array)
+{
+	int* first_row = array[0];
+	int width = 0;
+	while (first_row[width] != 0) {
+		width++;
+	}
+
+	return width;
+}
+
 /**
  * free_grid - function free 2D array
  * @grid: the array
@@ -8,12 +20,18 @@
 
 void free_grid(int **grid, int height)
 {
-	int i;
+	int i, j, width;
 
 	if (grid == NULL)
 		return;
-	for (i = 0; i < height; i++)
+	width = get_width(grid);
+
+	for (i = 0; i < width; i++)
 	{
+		for (j = 0; j < height; j++)
+		{
+			free(grid[j]);
+		}
 		free(grid[i]);
 	}
 		free(grid);
