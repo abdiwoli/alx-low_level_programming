bef86f2e3998e7de112e9e75bacc64018acd1b94
abdiwoli <abdiwolih@gmail.com>
2023-07-10 16:06:00 +0300

    done

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function that returns pointer to wto demonistion array
+ * @width: the width od the array
+ * @height: the height
+ * Return: array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **ptr;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(int *) * width);
+	for (i = 0; i < width; i++)
+	{
+		ptr[i] = malloc(sizeof(int *) * height);
+		for (j = 0; j < height; j++)
+			ptr[i][j] = 0;
+	}
+
+	return (ptr);
+}
