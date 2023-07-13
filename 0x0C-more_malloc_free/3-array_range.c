c5514f1b692bcd41fd82abf650866b2b7ffb6336
abdiwoli <abdiwolih@gmail.com>
2023-07-14 02:16:29 +0300

    abdi

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,14 +12,14 @@ int *array_range(int min, int max)
 	int i, len = max - min + 2;
 	int *ptr;
 
-	if(min > max)
+	if (min > max)
 		return (NULL);
 	ptr = malloc(sizeof(int) * len);
 
 	if (!ptr)
 		return (NULL);
-	for (i = 0; i < len; i++)
+	for (i = min; i < (max + 1); i++)
 		ptr[i] = i;
-	ptr[len] = '\0';
+	ptr[max + 1] = '\0';
 	return (ptr);
 }
