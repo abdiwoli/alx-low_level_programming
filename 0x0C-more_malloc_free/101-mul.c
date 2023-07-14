b6997da24fcf378185727aafc33bc87533a2c668
abdiwoli <abdiwolih@gmail.com>
2023-07-14 21:53:19 +0300

    done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,14 +9,14 @@
  */
 int main(int argc, char **argv)
 {
-	int num1;
-	int num2;
+	unsigned long num1;
+	unsigned long num2;
 
 	if (argc < 3)
 		exit(98);
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 
-	printf("%d\n", num1 * num2);
+	printf("%lu\n", num1 * num2);
 	return (0);
 }
