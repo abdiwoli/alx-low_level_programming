6a7c22af947eb7b6c567b2688eac0b4af868d519
abdiwoli <abdiwolih@gmail.com>
2023-07-17 14:11:31 +0300

    last

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,10 +7,10 @@
 
 void print_dog(struct dog *d)
 {
-	printf("%s\n", d->name == NULL || d->name[0] == '\0' ? "nill" : d->name);
+	printf("%s\n", d->name == NULL || d->name[0] == '\0' ? "nil" : d->name);
 	if (d->age <= 0.0)
-		printf("nill\n");
+		printf("nil\n");
 	else
 		printf("%f\n", d->age);
-	printf("%s\n", d->owner == NULL || d->owner[0] == '\0' ? "nill" : d->owner);
+	printf("%s\n", d->owner == NULL || d->owner[0] == '\0' ? "nil" : d->owner);
 }
