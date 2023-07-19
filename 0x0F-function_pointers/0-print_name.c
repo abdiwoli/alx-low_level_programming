6424a651979df951b2065c521c48be0984654758
abdiwoli <abdiwolih@gmail.com>
2023-07-19 17:39:40 +0300

    done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function prints name
+ * @name: the name
+ * @f: function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+read me file for 0x0F-function_pointers
+alx low level prgramming
