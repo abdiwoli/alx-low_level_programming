98a45b991a30108ec1e4bb4eeaf8925d2a31baf1
abdiwoli <abdiwolih@gmail.com>
2023-07-20 01:19:54 +0300

    done

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: the size
+ * @argv: the arguments array
+ * Return: 0 for success
+ */
+
+int main(int argc, char **argv)
+{
+	int num1, num2;
+	char *opr;
+	int (*fun)(int, int);
+
+
+	if (argc == 4)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[3]);
+		opr = argv[2];
+		fun = get_op_func(opr);
+		printf(" result %d\n", fun(num1, num2));
+	}
+	return (0);
+}
