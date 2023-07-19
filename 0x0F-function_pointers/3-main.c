ba8ab8aca5a61f2006271752356657af04228556
abdiwoli <abdiwolih@gmail.com>
2023-07-20 01:59:38 +0300

    done

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,6 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * main - main function
  * @argc: the size
@@ -12,31 +11,29 @@
 int main(int argc, char **argv)
 {
 	int num1, num2;
-	char *opr;
+	char *p;
 	int (*fun)(int, int);
 
 	if (argc == 4)
 	{
 		opr = argv[2];
-		if (*opr != '/' && *opr != '%' && *opr != '-' && *opr != '+' && *opr != '*' && strlen(opr) != 1)
+		if (*p != '/' && *p != '%' && *p != '-' && *p != '+' && *p != '*')
 		{
 			printf("Error\n");
-			exit(98);
+			exit(99);
 		}
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[3]);
-		if (num2 == 0 && *opr == '/')
+		if (num2 == 0 && *p == '/')
 		{
 			printf("Error\n");
 			exit(100);
 		}
-		fun = get_op_func(opr);
+		fun = get_op_func(p);
 		printf("%d\n", fun(num1, num2));
+		return (0);
 	}
-	else
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	return (0);
+	printf("Error\n");
+	exit(98);
+	return (1);
 }
