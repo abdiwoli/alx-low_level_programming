43e22be4dff7dfd63f5f4dc8ae803eee6ddf0e62
abdiwoli <abdiwolih@gmail.com>
2023-07-20 15:14:45 +0300

    done

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,7 +11,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list list;
-	char *s = separator;
+	const char *s = separator;
 
 	va_start(list, n);
 	for (i = 0; i < n; i++)
