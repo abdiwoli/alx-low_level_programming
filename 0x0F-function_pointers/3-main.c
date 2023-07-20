12b4d0b2274e30792128d2ea23891ebba4a830e9
abdiwoli <abdiwolih@gmail.com>
2023-07-20 23:02:56 +0300

    done

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,11 +17,6 @@ int main(int argc, char **argv)
 	if (argc == 4 && argv != NULL)
 	{
 		p = argv[2];
-		while (*(p + 1))
-		{
-			printf("Error\n");
-			exit(99);
-		}
 		if (*p != '/' && *p != '%' && *p != '-' && *p != '+' && *p != '*')
 		{
 			printf("Error\n");
