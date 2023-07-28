486262584f37e0bb249349cc124a931e1c07ad2a
abdiwoli <abdiwolih@gmail.com>
2023-07-28 18:55:41 +0300

    doneQ0

---
+++ 0x12-singly_linked_list/0-print_list.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * _strlen - returns length of param
+ * @s: string pram
+ * Return: int type
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - function prints elements of linked list
+ * @h: linked list
+ * Return:  size t
+ */
+size_t print_list(const list_t *h)
+{
+	int i;
+	char *s;
+
+	for (i = 0; h != NULL; i++)
+	{
+		s = h->str;
+		if (s == NULL)
+			s = "(nil)";
+		printf("[%d] %s\n", _strlen(s), s);
+		h = h->next;
+	}
+	return (i);
+}
