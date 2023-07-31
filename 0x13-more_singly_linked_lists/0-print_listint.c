f876f5707e6ad6c41eb17e7979492cd43354c3a1
abdiwoli <abdiwolih@gmail.com>
2023-07-31 23:06:51 +0300

    done

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_listint - function prints int
+ * @h: linked list paramater
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t n, num;
+
+	while (h)
+	{
+		n = h->n;
+		putchar('0' + n);
+		putchar('\n');
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+alx low level programming projects 0x13-more_singly_linked_lists
