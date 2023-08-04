078e664e0c0b9fb381a1192d15e44206321e8a1b
abdiwoli <abdiwolih@gmail.com>
2023-08-04 20:00:02 +0300

    done

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,9 +8,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL)
+	if (n == NULL || index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	*n |= (0 << index);
+	*n &= ~(1ul << index);
 	if (n)
 		return (1);
 	return (-1);
