9a91113f81fd33389082c17e2498cde79042b529
abdiwoli <abdiwolih@gmail.com>
2023-08-07 21:49:10 +0300

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,40 +1,44 @@
-#include "main.h"
 #include <stdio.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <stdlib.h>
 /**
- * main - function copies file
- * @argc: the size of arguments
- * @argv: the arguments
- * Return: 0 onsucess
+ * main - function copied file
+ * @argc: the size of the arguments
+ * @argv: array
+ * Return: 10 on succes
  */
-
 int main(int argc, char *argv[])
 {
-	char c, *str, *st;
-	FILE *file_from, *file_to;
+	char c, *st, *str;
+	FILE *file_from;
+	int fd_to;
+	mode_t m = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc < 3)
 		exit(98);
+
 	st = argv[1];
 	str = argv[2];
 	file_from = fopen(st, "r");
 
 	if (file_from == NULL)
 		exit(98);
-	file_to = fopen(str, "w");
 
-	if (file_to != NULL)
+	fd_to = open(str, O_CREAT | O_WRONLY | O_TRUNC, m);
+	if (fd_to == -1)
 	{
 		fclose(file_from);
 		exit(98);
 	}
+
 	while ((c = fgetc(file_from)) != EOF)
-	{
-		fputc(c, file_to);
-	}
+		write(fd_to, &c, 1);
+
 	fclose(file_from);
-	fclose(file_to);
+	close(fd_to);
 
 	return (0);
 }
