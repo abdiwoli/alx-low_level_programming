4c2acaa6a4502bd8aa2aa18741482a4b99eea5c1
abdiwoli <abdiwolih@gmail.com>
2023-08-08 17:54:04 +0300

    do

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,15 +6,6 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * erorf - function prints error
- * @str: the path
- */
-void erorf(char *str)
-{
-	fprintf(stderr, "Error: can't read from file %s\n", str);
-	exit(98);
-}
-/**
  * erorm - function prints error
  * @str: the path
  */
@@ -31,37 +22,36 @@ void erorm(char *str)
  */
 int main(int argc, char *argv[])
 {
-	char *source = argv[1], *dest = argv[2], buffer[1024];
-	ssize_t let, n;
-	int fd = open(source, O_RDONLY), fd_from, fd_close;
+	char *st = argv[1], *str = argv[2], buffer[1024];
+	FILE *file_from;
+	int fd_to, fd_close;
+	ssize_t bytesRead;
+	mode_t m = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc < 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (fd == -1)
-		erorf(source);
-	let = read(fd, buffer, sizeof(buffer));
-	if (let == -1)
-		erorf(source);
-
-	n = write(STDOUT_FILENO, buffer, let);
-	if (n == -1)
-		erorf(source);
-	fd_from = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-	if (fd_from == -1)
-		erorm(dest);
-	n =  write(fd_from, buffer, let);
-	if (n == -1)
-		erorm(dest);
-	fd_close = close(fd_from);
+	file_from = fopen(argv[1], "r");
+	if (file_from == NULL)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", st);
+		exit(98);
+	}
+	fd_to = open(str, O_CREAT | O_WRONLY | O_TRUNC, m);
+	if (fd_to == -1)
+		erorm(str);
+	while ((bytesRead = fread(buffer, 1, sizeof(buffer), file_from)) > 0)
+		if (write(fd_to, buffer, bytesRead) != bytesRead)
+			erorm(str);
+	fd_close = fclose(file_from);
 	if (fd_close != 0)
 	{
 		fprintf(stderr, "Error: Can't close fd %d", fd_close);
 		exit(100);
 	}
-	fd_close = close(fd);
+	fd_close = close(fd_to);
 	if (fd_close != 0)
 	{
 		fprintf(stderr, "Error: Can't close fd %d", fd_close);
