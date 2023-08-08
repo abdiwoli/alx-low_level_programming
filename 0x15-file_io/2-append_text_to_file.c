fd23b18137f962c8f9490abd97c859d0359a529b
abdiwoli <abdiwolih@gmail.com>
2023-08-08 09:00:33 +0300

    done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,6 +18,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (ptr == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
 	fprintf(ptr, "%s", text_content);
 	fclose(ptr);
 	return (1);
