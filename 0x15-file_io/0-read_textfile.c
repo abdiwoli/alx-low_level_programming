60344f2dbec935f14d734d4f3c5efc387166916a
abdiwoli <abdiwolih@gmail.com>
2023-08-08 08:01:19 +0300

    return

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t i = 0, n;
+	size_t i = 0;
 	char c;
 	FILE *ptr = fopen(filename, "r");
 
@@ -25,11 +25,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 			return (i);
 		}
 		n = write(1, &c, 1);
-	        if (n != 1)
-		{
-			fclose(ptr);
-			return (0);
-		}
 		c = fgetc(ptr);
 	}
 	fclose(ptr);
