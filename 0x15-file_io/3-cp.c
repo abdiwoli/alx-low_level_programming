7e38cc20243ecb1af5c2b5845ec738b6563ff4b3
abdiwoli <abdiwolih@gmail.com>
2023-08-08 11:09:09 +0300

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,47 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <sys/types.h>
+#include <unistd.h>
 #include <sys/stat.h>
-/**
- * newfile_write - function helps main
- * @file_from: file
- * @str: the file name
- * Return: 0
- */
-int newfile_write(FILE *file_from, char *str)
-{
-	char buffer[1024];
-	ssize_t bytesRead;
-	mode_t m = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	int fd_to, fd_close;
-
-	fd_to = open(str, O_CREAT | O_WRONLY | O_TRUNC, m);
-	if (fd_to == -1)
-	{
-		fprintf(stderr, "Error: Can't create or write to file %s\n", str);
-		fclose(file_from);
-		exit(99);
-	}
-	while ((bytesRead = fread(buffer, 1, sizeof(buffer), file_from)) > 0)
-	{
-		if (write(fd_to, buffer, bytesRead) != bytesRead)
-		{
-			fprintf(stderr, "Error: Can't write to file %s\n", str);
-			fclose(file_from);
-			close(fd_to);
-			exit(99);
-		}
-	}
-	fd_close = close(fd_to);
-	if (fd_close != 0)
-	{
-		fprintf(stderr, "Error: Error: Can't close fd %s\n", str);
-		exit(100);
-	}
-	return (0);
-}
+#include <sys/types.h>
+#include <stdlib.h>
 /**
  * main - function copied file
  * @argc: the size of the arguments
@@ -50,29 +12,56 @@ int newfile_write(FILE *file_from, char *str)
  */
 int main(int argc, char *argv[])
 {
-	char *st = argv[1], *str = argv[2];
+	char *st, *str;
 	FILE *file_from;
-	int fd_close;
+	int fd_to, fd_close;
+	char buffer[1024];
+	ssize_t bytesRead;
+	mode_t m = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
-	dup2(STDOUT_FILENO, STDERR_FILENO);
-
-	if (argc != 3)
+	if (argc < 3)
 	{
-		fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
+		fprintf(stderr, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
+	st = argv[1];
+	str = argv[2];
 	file_from = fopen(st, "r");
+
 	if (file_from == NULL)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", st);
+		fprintf(stderr, "Error: Can't read from file NAME_OF_THE_FILE %s\n", st);
 		exit(98);
 	}
-	newfile_write(file_from, str);
+
+	fd_to = open(str, O_CREAT | O_WRONLY | O_TRUNC, m);
+	if (fd_to == -1)
+	{
+		fclose(file_from);
+		fprintf(stderr, "Error: Can't write %s\n", str);
+		exit(99);
+	}
+
+	while ((bytesRead = fread(buffer, 1, sizeof(buffer), file_from)) > 0) {
+		if (write(fd_to, buffer, bytesRead) != bytesRead) {
+			fprintf(stderr, "Error: Can't write %s\n", str);
+			fclose(file_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
+
 	fd_close = fclose(file_from);
 	if (fd_close != 0)
 	{
-		fprintf(stderr, "Error: Error: Can't close fd %s\n", st);
+		fprintf(stderr, "Error: Can't close fd %d", fd_close);
+		exit(100);
+	}
+	fd_close = close(fd_to);
+	if (fd_close != 0)
+	{
+		fprintf(stderr, "Error: Can't close fd %d", fd_close);
 		exit(100);
 	}
 	return (0);
