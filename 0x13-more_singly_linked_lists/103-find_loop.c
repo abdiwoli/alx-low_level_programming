f044c630f5281c4270743648fcc0c0f34577f38d
abdiwoli <abdiwolih@gmail.com>
2023-08-31 02:39:05 +0300

    done

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -3,31 +3,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_listint_safe - function prints safe
+ * find_listint_loop - function prints safe
  * Return:n
  * @head: the list
  */
 listint_t *find_listint_loop(listint_t *head)
 {
+	listint_t *arr[1024];
 	int i, n = 0;
 
 	if (head == NULL)
 		return (0);
-	while (head)
+	while (head && i < 1024)
 	{
 		for (i = 0; i < n; i++)
 		{
 			if (head == arr[i])
 			{
-				flag = 1;
-				return ((void *)head);
+				return (arr[i]);
 			}
 		}
 
-		printf("[%p] %d\n", (void *)head, head->n);
+		arr[n] = head;
 		n++;
 		head = head->next;
 	}
-
 	return (NULL);
 }
