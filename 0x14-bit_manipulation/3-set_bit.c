e6d26c2ce050660eb95f7084fded494ddd039b14
abdiwoli <abdiwolih@gmail.com>
2023-08-31 15:44:36 +0300

    run

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * set_bit - function sets 1 bit
+ * @n: the pointer to n
+ * @index: index to set
+ * Return: 1 for success
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int one = 1ul;
+
+	if (n == NULL)
+		return (-1);
+
+	*n |= (one << index);
+	if (n)
+		return (1);
+	return (-1);
+}
