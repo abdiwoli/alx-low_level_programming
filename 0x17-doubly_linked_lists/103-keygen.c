b05fb030525a1b159d80b3ce6265babea1547926
abdiwoli <abdiwolih@gmail.com>
2023-10-12 23:15:00 +0300

    done

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -13,7 +13,10 @@ int main(int argc, char **argv)
 	if (argc == 2)
 	{
 		argv[1] = "Congrats!";
-		printf("%s", argv[1]);
+		printf("%s\n", argv[1]);
+		return (0);
 	}
-	return (0);
+	argv[0] = "Segmentation fault (core dumped)";
+	printf("%s\n", argv[0]);
+	return (1);
 }
