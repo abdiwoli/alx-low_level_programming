702ac3daab190630499489bc4ef33e4352aead54
abdiwoli <abdiwolih@gmail.com>
2023-10-12 22:51:58 +0300

    done

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/**
-  * main - function
-  * @argc: array
-  * @argv: has the arguments
-  * Return: 0 for success
-  */
 
-int main(int argc, char **argv)
+unsigned int simple_hash(const char *str)
 {
-	if (argc == 2)
-		printf("%s", argv[1]);
-	return (0);
+    unsigned int hash = 5381;
+    int c;
+
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c;
+    }
+
+    return hash;
+}
+
+void generate_key(const char *username, char *key)
+{
+    sprintf(key, "%u", simple_hash(username));
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s username\n", argv[0]);
+        return 1;
+    }
+
+    char key[20];
+
+    generate_key(argv[1], key);
+
+    printf("Key for %s: %s\n", argv[1], key);
+
+    return 0;
 }
