ddbae7355fd9e86271a181f40081a00b652b0548
abdiwoli <abdiwolih@gmail.com>
2023-11-03 23:06:28 +0300

    fix4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * hash_table_get - function searches the key
  * @ht: the table
@@ -9,13 +10,18 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *node;
 
 	index = key_index((const unsigned char *)key, (unsigned long int)ht->size);
 
-
 	if (ht == NULL || key == NULL)
 		return (NULL);
-	if (!(ht->array[index]))
-		return (NULL);
-	return (ht->array[index]->value);
+	node = ht->array[index];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
 }
