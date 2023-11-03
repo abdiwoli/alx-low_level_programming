a22881e5b6ac444783a80d41604c2aa75f64f214
abdiwoli <abdiwolih@gmail.com>
2023-11-03 10:53:55 +0300

    fix

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -10,7 +10,6 @@ unsigned long int hash_djb2(const unsigned char *str)
 	unsigned long int value = 5381;
 	int n;
 
-	
 	for  (; (n = *str); str++)
 		value = ((value << 5) + value) + n;
 	return (value);
