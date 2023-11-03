1dfe897f9eddaf25eb69669a3ca5c751bba5a1d1
abdiwoli <abdiwolih@gmail.com>
2023-11-03 10:29:17 +0300

    added1

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - function name
+ * @str: the key
+ * Return: index
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int value = 7919;
+	int n;
+
+	n = *str++;
+	while (n)
+	{
+		value = ((value << 5) + value) + n;
+		n = *str++;
+	}
+	return (value);
+}
