29b7a5d7e8ff4fdfd496338babf93c1dc3ece6db
abdiwoli <abdiwolih@gmail.com>
2023-11-03 22:52:39 +0300

    fix3and4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,7 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *current;
 
 	if (ht == NULL || key == NULL || value == NULL)
-		return (0)
+		return (0);
 	index = key_index((const unsigned char *)key, (unsigned long int)ht->size);
 	current =  ht->array[index];
 	while (current != NULL)
